28ee931e30969200b34a4e169c03d47638903265
Daniel Alkemic Czuba <alkemic7@gmail.com>
2014-05-06 19:35:56 +0200

    Handling media

--- dc/urls.py
+++ dc/urls.py
@@ -1,6 +1,7 @@
 #-*- coding:utf-8 -*-
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 from django.views.generic.base import RedirectView, TemplateView
 
 from boski.views.base import TemplateTextPlainView
@@ -9,6 +10,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^robots\.txt$', TemplateTextPlainView.as_view(template_name='robots.txt')),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     url(r'^$', TemplateTextPlainView.as_view(template_name='wip.txt'), name='index'),
     url(r'^$', RedirectView.as_view(pattern_name='blog:index'), name='index'),
     (r'about/', include('module.about.urls', namespace='about')),
@@ -16,7 +18,6 @@ urlpatterns = patterns('',
     (r'^blog/', include('module.blog.urls', namespace='blog')),
     (r'^', include('module.static_page.urls', namespace='static_page')),
     (r'portfolio/', include('module.portfolio.urls', namespace='portfolio')),
-    (r'cv.test', TemplateView.as_view(template_name='cv.html')),
     # url(r'^dc/', include('dc.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
