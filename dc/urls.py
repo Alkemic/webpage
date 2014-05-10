2e2d45bc9335e0c8e4bfd92f3ab22a922c88f4e0
Daniel Alkemic Czuba <alkemic7@gmail.com>
2014-05-10 22:34:44 +0200

    Conditional displaying of WIP index page

--- dc/urls.py
+++ dc/urls.py
@@ -2,28 +2,23 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
-from django.views.generic.base import RedirectView, TemplateView
+from django.views.generic.base import RedirectView
 
 from boski.views.base import TemplateTextPlainView
 
 admin.autodiscover()
 
+index_entry = url(r'^$', TemplateTextPlainView.as_view(template_name='wip.txt'), name='index') \
+    if getattr(settings, 'IS_WIP', False) \
+    else url(r'^$', RedirectView.as_view(pattern_name='blog:index'), name='index')
+
 urlpatterns = patterns('',
     url(r'^robots\.txt$', TemplateTextPlainView.as_view(template_name='robots.txt')),
+    index_entry,
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    url(r'^$', TemplateTextPlainView.as_view(template_name='wip.txt'), name='index'),
-    url(r'^$', RedirectView.as_view(pattern_name='blog:index'), name='index'),
-    (r'about/', include('module.about.urls', namespace='about')),
+    (r'^about/', include('module.about.urls', namespace='about')),
     (r'^cms/', include('module.cms.urls', namespace='cms')),
     (r'^blog/', include('module.blog.urls', namespace='blog')),
+    (r'^portfolio/', include('module.portfolio.urls', namespace='portfolio')),
     (r'^', include('module.static_page.urls', namespace='static_page')),
-    (r'portfolio/', include('module.portfolio.urls', namespace='portfolio')),
-    # url(r'^dc/', include('dc.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
-
